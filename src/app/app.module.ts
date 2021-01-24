61f3c8a28679c3528d1d46c6bdbe8108eca59f54
ShabanK <shabankhawar99@gmail.com>
2021-01-25 03:26:55 +0500

    generated a service and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
