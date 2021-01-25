dd5261118e70db0c19b5c5c76c16ae2502436f73
ShabanK <shabankhawar99@gmail.com>
2021-01-25 12:47:00 +0500

    completed app

--- src/app/fetch-image.service.ts
+++ src/app/fetch-image.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
-import {} from "dotenv"
+import { pluck } from 'rxjs/operator/pluck';
+// require("dotenv").config()
+// console.log(process.env.ACCESS_KEY)
+
+interface imageData{
+  urls:{
+    regular:string
+  }
+}
+
 
 @Injectable()
 export class FetchImageService {
@@ -8,7 +17,11 @@ export class FetchImageService {
   constructor(private http: HttpClient) { }
 
   getImages(){
-    this.http.get(`https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`)
+    return this.http.get<imageData>(`https://api.unsplash.com/photos/random`, {
+      headers: {
+        Authorization: `Client-ID e1Ap-eIxZlrgqU3gg1peqFv4N8UaoxyTp4Aa7VtO_AQ`
+      }
+    })
   }
 
 }
--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FetchImageService } from "../fetch-image.service"
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
-  constructor() { }
+  imageData:string;
+
+  constructor(private fetchImage: FetchImageService) { }
 
   ngOnInit() {
+    this.fetchImage.getImages().subscribe(imageData=>{
+      this.imageData=imageData.urls.regular
+      console.log(imageData.urls.regular)
+    })
+  }
+  refetch(){
+    this.fetchImage.getImages().subscribe(imageData=>{
+      this.imageData=imageData.urls.regular
+      console.log(imageData.urls.regular)
+    })
   }
 
 }
